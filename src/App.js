71fddbc199714c69f37c2340fd77c71b7b549449
palabrasafiladas <ailin41210254@gmail.com>
2023-01-20 14:38:09 -0300

    adding new components, structure basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoList } from './components/TodoList';
+import { TodoItem } from './components/TodoItem';
+import { CreateTodoButton } from './components/CreateTodoButton';
+const todos = 
+[
+{text: 'crear componente', completed: false},
+{text: 'renderizar componente', completed: false},
+{text: 'editar componente', completed: false},
+
+
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <TodoCounter/>
+    <TodoSearch/>
+    <TodoList>
+      {todos.map(todo =>(
+           <TodoItem key= {todo.text} text={todo.text}/>
+ ))
+      }
+    <TodoItem/>
+    </TodoList>
+    <CreateTodoButton/>
+    </React.Fragment>
+
   );
 }
 
---
+++ src/components/TodoCounter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoCounter() {
+
+
+    return (
+        <h2> Has completado 2 de 3</h2>
+
+    );
+}
+
+export {TodoCounter} ;
